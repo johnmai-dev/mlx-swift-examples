58ee8a5eda5d8cf43f7ad0b7e5a756ada6dd60e7
John Mai <maiqingqiang@foxmail.com>
2025-04-13 18:31:51 +0800

    multiple-evaluators

--- Applications/LLMEval/ContentView.swift
+++ Applications/LLMEval/ContentView.swift
@@ -1,11 +1,11 @@
 // Copyright © 2024 Apple Inc.
 
+import MarkdownUI
+import Metal
 import MLX
 import MLXLLM
 import MLXLMCommon
 import MLXRandom
-import MarkdownUI
-import Metal
 import SwiftUI
 import Tokenizers
 
@@ -13,6 +13,9 @@ struct ContentView: View {
     @Environment(DeviceStat.self) private var deviceStat
 
     @State var llm = LLMEvaluator()
+    @State var llm2 = LLMEvaluator2()
+
+    @State private var prompt = "Who are you?"
 
     enum displayStyle: String, CaseIterable, Identifiable {
         case plain, markdown
@@ -21,8 +24,9 @@ struct ContentView: View {
 
     @State private var selectedDisplayStyle = displayStyle.markdown
 
-    var body: some View {
-        VStack(alignment: .leading) {
+    @ViewBuilder
+    func LLMEvaluatorView() -> some View {
+        VStack {
             VStack {
                 HStack {
                     Text(llm.modelInfo)
@@ -48,7 +52,6 @@ struct ContentView: View {
                             Text(option.rawValue.capitalized)
                                 .tag(option)
                         }
-
                     }
                     .pickerStyle(.segmented)
                     #if os(visionOS)
@@ -58,7 +61,6 @@ struct ContentView: View {
                     #endif
                 }
             }
-
             // show the model output
             ScrollView(.vertical) {
                 ScrollViewReader { sp in
@@ -80,21 +82,91 @@ struct ContentView: View {
                         .id("bottom")
                 }
             }
+        }
+    }
+
+    @ViewBuilder
+    func LLMEvaluator2View() -> some View {
+        VStack {
+            VStack {
+                HStack {
+                    Text(llm2.modelInfo)
+                        .textFieldStyle(.roundedBorder)
+
+                    Spacer()
+
+                    Text(llm2.stat)
+                }
+                HStack {
+                    Toggle(isOn: $llm2.includeWeatherTool) {
+                        Text("Include \"get current weather\" tool")
+                    }
+                    .frame(maxWidth: 350, alignment: .leading)
+                    Spacer()
+                    if llm2.running {
+                        ProgressView()
+                            .frame(maxHeight: 20)
+                        Spacer()
+                    }
+                    Picker("", selection: $selectedDisplayStyle) {
+                        ForEach(displayStyle.allCases, id: \.self) { option in
+                            Text(option.rawValue.capitalized)
+                                .tag(option)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    #if os(visionOS)
+                        .frame(maxWidth: 250)
+                    #else
+                        .frame(maxWidth: 150)
+                    #endif
+                }
+            }
+            // show the model output
+            ScrollView(.vertical) {
+                ScrollViewReader { sp in
+                    Group {
+                        if selectedDisplayStyle == .plain {
+                            Text(llm2.output)
+                                .textSelection(.enabled)
+                        } else {
+                            Markdown(llm2.output)
+                                .textSelection(.enabled)
+                        }
+                    }
+                    .onChange(of: llm2.output) { _, _ in
+                        sp.scrollTo("bottom")
+                    }
+
+                    Spacer()
+                        .frame(width: 1, height: 1)
+                        .id("bottom")
+                }
+            }
+        }
+    }
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                LLMEvaluatorView()
+                LLMEvaluator2View()
+            }
 
             HStack {
-                TextField("prompt", text: Bindable(llm).prompt)
+                TextField("prompt", text: $prompt)
                     .onSubmit(generate)
-                    .disabled(llm.running)
-                    #if os(visionOS)
-                        .textFieldStyle(.roundedBorder)
-                    #endif
-                Button(llm.running ? "stop" : "generate", action: llm.running ? cancel : generate)
+                    .disabled(llm.running || llm2.running)
+                #if os(visionOS)
+                    .textFieldStyle(.roundedBorder)
+                #endif
+                Button(llm.running || llm2.running ? "stop" : "generate", action: llm.running || llm2.running ? cancel : generate)
             }
         }
         #if os(visionOS)
-            .padding(40)
+        .padding(40)
         #else
-            .padding()
+        .padding()
         #endif
         .toolbar {
             ToolbarItem {
@@ -125,16 +197,21 @@ struct ContentView: View {
                 .disabled(llm.output == "")
                 .labelStyle(.titleAndIcon)
             }
-
         }
         .task {
             // pre-load the weights on launch to speed up the first generation
             _ = try? await llm.load()
+            _ = try? await llm2.load()
         }
     }
 
     private func generate() {
-        llm.generate()
+        Task {
+            llm.generate(prompt: prompt)
+        }
+        Task {
+            llm2.generate(prompt: prompt)
+        }
     }
 
     private func cancel() {
@@ -154,12 +231,10 @@ struct ContentView: View {
 @Observable
 @MainActor
 class LLMEvaluator {
-
     var running = false
 
     var includeWeatherTool = false
 
-    var prompt = ""
     var output = ""
     var modelInfo = ""
     var stat = ""
@@ -227,9 +302,8 @@ class LLMEvaluator {
                 context.model.numParameters()
             }
 
-            self.prompt = modelConfiguration.defaultPrompt
-            self.modelInfo =
-                "Loaded \(modelConfiguration.id).  Weights: \(numParams / (1024*1024))M"
+            modelInfo =
+                "Loaded \(modelConfiguration.id).  Weights: \(numParams / (1024 * 1024))M"
             loadState = .loaded(modelContainer)
             return modelContainer
 
@@ -239,8 +313,7 @@ class LLMEvaluator {
     }
 
     private func generate(prompt: String) async {
-
-        self.output = ""
+        output = ""
         let userInput = UserInput(prompt: prompt)
 
         do {
@@ -249,13 +322,14 @@ class LLMEvaluator {
             // each time you generate you will get something new
             MLXRandom.seed(UInt64(Date.timeIntervalSinceReferenceDate * 1000))
 
-            try await modelContainer.perform { (context: ModelContext) -> Void in
+            try await modelContainer.perform { (context: ModelContext) in
                 let lmInput = try await context.processor.prepare(input: userInput)
                 let stream = try MLXLMCommon.generate(
-                    input: lmInput, parameters: generateParameters, context: context)
+                    input: lmInput, parameters: generateParameters, context: context
+                )
 
                 var tokenCount = 0
-                var lastEmissionTime: Date = Date()
+                var lastEmissionTime = Date()
                 var chunks = ""
 
                 for await result in stream {
@@ -291,13 +365,163 @@ class LLMEvaluator {
         }
     }
 
-    func generate() {
+    func generate(prompt: String) {
         guard !running else { return }
-        let currentPrompt = prompt
-        prompt = ""
         generationTask = Task {
             running = true
-            await generate(prompt: currentPrompt)
+            await generate(prompt: prompt)
+            running = false
+        }
+    }
+
+    func cancelGeneration() {
+        generationTask?.cancel()
+        running = false
+    }
+}
+
+@Observable
+@MainActor
+class LLMEvaluator2 {
+    var running = false
+
+    var includeWeatherTool = false
+
+    var output = ""
+    var modelInfo = ""
+    var stat = ""
+
+    /// This controls which model loads. `qwen2_5_1_5b` is one of the smaller ones, so this will fit on
+    /// more devices.
+    let modelConfiguration = LLMRegistry.llama3_2_1B_4bit
+
+    /// parameters controlling the output
+    let generateParameters = GenerateParameters(temperature: 0.6)
+    let maxTokens = 240
+    let updateInterval = 0.25
+
+    /// A task responsible for handling the generation process.
+    var generationTask: Task<Void, Error>?
+
+    enum LoadState {
+        case idle
+        case loaded(ModelContainer)
+    }
+
+    var loadState = LoadState.idle
+
+    let currentWeatherToolSpec: [String: any Sendable] =
+        [
+            "type": "function",
+            "function": [
+                "name": "get_current_weather",
+                "description": "Get the current weather in a given location",
+                "parameters": [
+                    "type": "object",
+                    "properties": [
+                        "location": [
+                            "type": "string",
+                            "description": "The city and state, e.g. San Francisco, CA",
+                        ] as [String: String],
+                        "unit": [
+                            "type": "string",
+                            "enum": ["celsius", "fahrenheit"],
+                        ] as [String: any Sendable],
+                    ] as [String: [String: any Sendable]],
+                    "required": ["location"],
+                ] as [String: any Sendable],
+            ] as [String: any Sendable],
+        ] as [String: any Sendable]
+
+    /// load and return the model -- can be called multiple times, subsequent calls will
+    /// just return the loaded model
+    func load() async throws -> ModelContainer {
+        switch loadState {
+        case .idle:
+            // limit the buffer cache
+            MLX.GPU.set(cacheLimit: 20 * 1024 * 1024)
+
+            let modelContainer = try await LLMModelFactory.shared.loadContainer(
+                configuration: modelConfiguration
+            ) {
+                [modelConfiguration] progress in
+                Task { @MainActor in
+                    self.modelInfo =
+                        "Downloading \(modelConfiguration.name): \(Int(progress.fractionCompleted * 100))%"
+                }
+            }
+            let numParams = await modelContainer.perform { context in
+                context.model.numParameters()
+            }
+
+            modelInfo =
+                "Loaded \(modelConfiguration.id).  Weights: \(numParams / (1024 * 1024))M"
+            loadState = .loaded(modelContainer)
+            return modelContainer
+
+        case .loaded(let modelContainer):
+            return modelContainer
+        }
+    }
+
+    private func generate(prompt: String) async {
+        output = ""
+        let userInput = UserInput(prompt: prompt)
+
+        do {
+            let modelContainer = try await load()
+
+            // each time you generate you will get something new
+            MLXRandom.seed(UInt64(Date.timeIntervalSinceReferenceDate * 1000))
+
+            try await modelContainer.perform { (context: ModelContext) in
+                let lmInput = try await context.processor.prepare(input: userInput)
+                let stream = try MLXLMCommon.generate(
+                    input: lmInput, parameters: generateParameters, context: context
+                )
+
+                var tokenCount = 0
+                var lastEmissionTime = Date()
+                var chunks = ""
+
+                for await result in stream {
+                    switch result {
+                    case .chunk(let string):
+                        tokenCount += 1
+                        if tokenCount >= maxTokens { await generationTask?.cancel() }
+                        let now = Date()
+                        if now.timeIntervalSince(lastEmissionTime) >= updateInterval {
+                            lastEmissionTime = now
+                            let text = chunks
+                            chunks = ""
+                            Task { @MainActor in
+                                self.output += text
+                            }
+                        } else {
+                            chunks += string
+                        }
+                    case .info(let info):
+                        Task { @MainActor in
+                            self.stat = "\(info.tokensPerSecond) tokens/s"
+                        }
+                    }
+                }
+
+                Task { @MainActor in
+                    self.output += chunks
+                }
+            }
+
+        } catch {
+            output = "Failed: \(error)"
+        }
+    }
+
+    func generate(prompt: String) {
+        guard !running else { return }
+        generationTask = Task {
+            running = true
+            await generate(prompt: prompt)
             running = false
         }
     }
